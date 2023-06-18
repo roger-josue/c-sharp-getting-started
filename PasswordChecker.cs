bdaa5037ee449adb659b2ac5d83eb945c1f33f82
roger-josue <jovz1798@gmail.com>
2023-06-18 13:21:01 -0600

    password checker conditional project added, focused solely on conditional, nothing fancy regarding oop, just plain variables (fields) without any properties

---
+++ PasswordChecker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace c_sharp_getting_started
+{
+	public class PasswordChecker
+	{
+		public PasswordChecker()
+		{
+            int minLength = 8;
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string uppercase = lowercase.ToUpper();
+            string digits = "0123456789";
+            string specialChars = @"!@#$%^&*()-+=/?><.,{}';:`~";
+
+            string password = Console.ReadLine();
+            int score = 0;
+            if (password.Length >= minLength)
+            {
+                score += 1;
+            }
+            if (Contains(password, uppercase) == true)
+            {
+                score += 1;
+            }
+            if (Contains(password, lowercase) == true)
+            {
+                score += 1;
+            }
+            if (Contains(password, digits) == true)
+            {
+                score += 1;
+            }
+            if (Contains(password, specialChars) == true)
+            {
+                score += 1;
+            }
+            switch (score)
+            {
+                case 5:
+                case 4:
+                    Console.WriteLine("Password strenght: extremely strong");
+                    break;
+                case 3:
+                    Console.WriteLine("Password strenght: strong");
+                    break;
+                case 2:
+                    Console.WriteLine("Password strenght: medium");
+                    break;
+                case 1:
+                    Console.WriteLine("Password strenght: weak");
+                    break;
+                default:
+                    Console.WriteLine("Password strenght: Not a valid password, does not meet the criteria");
+                    break;
+
+            }
+        }
+
+        public static bool Contains(string target, string list)
+        {
+            return target.IndexOfAny(list.ToCharArray()) != -1;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -17,44 +17,44 @@ internal class Program
         Console.WriteLine(b.Describe());
     }
 
-    static void Madlib()
-    {
-        // Give the Mad Lib a title:
-        string title = "That\'s odd";
+    // static void Madlib()
+    // {
+    //     // Give the Mad Lib a title:
+    //     string title = "That\'s odd";
 
-        Console.WriteLine(title);
-        // Define user input and variables:
-        Console.WriteLine("Hi, sit tight, Mad Libs game starting now!");
-        Console.WriteLine("Give me a name for the character and hit enter:");
-        string name = Console.ReadLine();
-        Console.WriteLine("Now, I want you to give me 3 different adjectives and hit enter every time you input each of them:");
-        string firstAdjective = Console.ReadLine();
-        string secondAdjective = Console.ReadLine();
-        string thirdAdjective = Console.ReadLine();
-        Console.WriteLine("What about a verb? Input one and hit enter:");
-        string verb = Console.ReadLine();
-        Console.WriteLine("Now, I want you to give me 2 different nouns and hit enter every time you input each of them:");
-        string firstNoun = Console.ReadLine();
-        string secondNoun = Console.ReadLine();
-        Console.WriteLine("An animal:");
-        string animal = Console.ReadLine();
-        Console.WriteLine("A food:");
-        string food = Console.ReadLine();
-        Console.WriteLine("A fruit:");
-        string fruit = Console.ReadLine();
-        Console.WriteLine("A superhero:");
+    //     Console.WriteLine(title);
+    //     // Define user input and variables:
+    //     Console.WriteLine("Hi, sit tight, Mad Libs game starting now!");
+    //     Console.WriteLine("Give me a name for the character and hit enter:");
+    //     string name = Console.ReadLine();
+    //     Console.WriteLine("Now, I want you to give me 3 different adjectives and hit enter every time you input each of them:");
+    //     string firstAdjective = Console.ReadLine();
+    //     string secondAdjective = Console.ReadLine();
+    //     string thirdAdjective = Console.ReadLine();
+    //     Console.WriteLine("What about a verb? Input one and hit enter:");
+    //     string verb = Console.ReadLine();
+    //     Console.WriteLine("Now, I want you to give me 2 different nouns and hit enter every time you input each of them:");
+    //     string firstNoun = Console.ReadLine();
+    //     string secondNoun = Console.ReadLine();
+    //     Console.WriteLine("An animal:");
+    //     string animal = Console.ReadLine();
+    //     Console.WriteLine("A food:");
+    //     string food = Console.ReadLine();
+    //     Console.WriteLine("A fruit:");
+    //     string fruit = Console.ReadLine();
+    //     Console.WriteLine("A superhero:");
 
-        string superhero = Console.ReadLine();
-        Console.WriteLine("A country:");
-        string country = Console.ReadLine();
-        Console.WriteLine("A dessert:");
-        string dessert = Console.ReadLine();
-        Console.WriteLine("A year:");
-        string year = Console.ReadLine();
-        // The template for the story:
+    //     string superhero = Console.ReadLine();
+    //     Console.WriteLine("A country:");
+    //     string country = Console.ReadLine();
+    //     Console.WriteLine("A dessert:");
+    //     string dessert = Console.ReadLine();
+    //     Console.WriteLine("A year:");
+    //     string year = Console.ReadLine();
+    //     // The template for the story:
 
-        string story = $"This morning {name} woke up feeling {firstAdjective}. 'It is going to be a {secondAdjective} day!' Outside, a bunch of {animal}s were protesting to keep {food} in stores. They began to {verb} to the rhythm of the {firstNoun}, which made all the {fruit}s very {thirdAdjective}. Concerned, {name} texted {superhero}, who flew {name} to {country} and dropped {name} in a puddle of frozen {dessert}. {name} woke up in the year {year}, in a world where {secondNoun}s ruled the world.";
-    }
+    //     string story = $"This morning {name} woke up feeling {firstAdjective}. 'It is going to be a {secondAdjective} day!' Outside, a bunch of {animal}s were protesting to keep {food} in stores. They began to {verb} to the rhythm of the {firstNoun}, which made all the {fruit}s very {thirdAdjective}. Concerned, {name} texted {superhero}, who flew {name} to {country} and dropped {name} in a puddle of frozen {dessert}. {name} woke up in the year {year}, in a world where {secondNoun}s ruled the world.";
+    // }
 
     static void Moneymaker()
     {
